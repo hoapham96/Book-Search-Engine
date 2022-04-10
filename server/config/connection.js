e96297cc818a1f415c47584fc18b484f51b89843
hoapham96 <89959754+hoapham96@users.noreply.github.com>
2022-04-10 01:54:43 -0500

    removed logs that caused error

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,11 +1,8 @@
 const mongoose = require('mongoose');
 
-const db = mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/googlebooks', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/googlebooks', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
-db.on("error", console.error.bind(console, "connection error: "));
-db.once("open", function () {
-  console.log("Connected successfully");
-});
+
 module.exports = mongoose.connection;
