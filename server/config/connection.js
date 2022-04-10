233978e17de3ea44fbafcec85f1f4101bbc9bd07
hoapham96 <89959754+hoapham96@users.noreply.github.com>
2022-04-10 02:13:35 -0500

    change IP

--- server/config/connection.js
+++ server/config/connection.js
@@ -3,6 +3,8 @@ const mongoose = require('mongoose');
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/googlebooks', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
-});
+}).then(() => {
+  console.log("Connection successfull");
+}).catch((e) => console.log("No connection"));
 
 module.exports = mongoose.connection;
